d7d456a8a0774612e7148f7094ff63941a9cd619
brandon-castro98 <jonathan_works98@outlook.com>
2022-09-27 16:13:28 -0500

    'Se agrego funcionamiento de tarea completada y borrar tarea lista'

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let idCounter = 0;
 const input = document.querySelector('input[type= "text"]');
 
-userInput.addEventListener('submit', (event)=> {
+userInput.addEventListener("submit", (event) => {
     event.preventDefault();
-   addTask(); 
+    addTask();
 });
 
-let addTask = ()=>{
+let addTask = () => {
     idCounter++;
     let newValue = input.value;
-    
+
     list.innerHTML += `
     <div class="tasks-container" id='${idCounter}'>
         <label for="input-task">
@@ -17,13 +17,27 @@ let addTask = ()=>{
             ${newValue}
             </label>
             <img src="/delete.svg" class="delete-icon">
-    </div>`
-    input.value = '';
+    </div>`;
+    input.value = "";
     updateStats();
 };
 
-let updateStats = ()=>{
-    let element = list.querySelectorAll('div');
-    stats.innerHTML = `<p>Tareas pendientes: ${element.length} Completadas: 3</p>`
+list.addEventListener("click", (event) => {
+    if (event.srcElement.nodeName == "INPUT") {
+        updateStats();
+    } else if (event.srcElement.nodeName == "IMG") {
+        deleteTask(event.srcElement.parentNode.id);
+    }
+});
 
+let updateStats = () => {
+    let element = list.querySelectorAll("div");
+    let checkbox = list.querySelectorAll('input[type="checkbox"]:checked');
+    stats.innerHTML = `<p>Tareas pendientes: ${element.length} Completadas: ${checkbox.length}</p>`;
+};
+
+let deleteTask = (id) => {
+    let taskToDelete = document.getElementById(id);
+    list.removeChild(taskToDelete);
+    updateStats();
 };
